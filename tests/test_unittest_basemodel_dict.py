d354be87b0deef6d5f242c1c61a2316efcb22ee7
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-15 05:30:22 -0700

    ran unittests (4) ok

--- tests/test_unittest_basemodel_dict.py
+++ tests/test_unittest_basemodel_dict.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from models.base_model import BaseModel
-
+from datetime import datetime
 
 class TestBaseModelDict(unittest.TestCase):
     def test_recreation_from_dict(self):
@@ -13,7 +13,8 @@ class TestBaseModelDict(unittest.TestCase):
         my_model_json = my_model.to_dict()
 
 # Recreating instance fromt eh dict representatikn
-        my_new_model = BaseModel(**my_model_json)
+        my_new_model = BaseModel(**my_model.to_dict())
+        my_new_model_name = "My First Model"
 
 # Check if recreated instance matches original
         self.assertEqual(my_model.id, my_new_model.id)
