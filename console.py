e53d90f2b541b2801a2e2c6b756632f5a35df9c9
sara0411 <saraferraa0@gmail.com>
2023-10-15 13:56:19 +0100

    console.py

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import cmd
+
+"""AirBnB console """
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
