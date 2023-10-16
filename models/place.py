057213e2061268a3a45c6bb3900e5283916c1dc4
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-15 18:16:22 -0700

    the branch

--- models/place.py
+++ models/place.py
@@ -9,10 +9,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = 0
-    number_of_rooms = 0
-    number_of_bathrooms = ""
-    maximum_guests = 0
-    price_per_night = 0
-    latitude = 0
-    longitude = 0
-    amenities = []
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
