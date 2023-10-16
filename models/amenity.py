d6fa47494e782d18bf7e935fa5147ce600297549
sara0411 <saraferraa0@gmail.com>
2023-10-16 01:51:21 +0100

    task 9

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines Amenity Class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class saves the  attibutes of an amenity:
+        - name (string)
+    """
+    name = ""
+
