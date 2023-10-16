36fed9804ee6d657028ca0615f66e8c655736430
sara0411 <saraferraa0@gmail.com>
2023-10-16 01:57:46 +0100

    task 9  adding tests

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines user module"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class  attributes:
+        - email (string)
+        - password (string)
+        - first_name (string)
+        - last_name (string)
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
